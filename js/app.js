faa9f4d69be07d8c4d30e6e26a1f22dfb8079c65
scrumptious <ljasiurski@gmail.com>
2017-03-28 21:57:48 +0100

    first version of generator

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('generatorApp', []);
+app.controller('canvasController', ['$scope', function canvasController($scope) {
+
+}]);
+app.controller('formController', ['$scope', function($scope) {
+		
+}]);
