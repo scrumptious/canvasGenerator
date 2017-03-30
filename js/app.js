e81e237924d6c41fa854fd344abe1eb652df1ab6
scrumptious <ljasiurski@gmail.com>
2017-03-30 20:45:19 +0100

    modified form and added generating random amount of shapes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,55 @@
 var app = angular.module('generatorApp', []);
-app.controller('canvasController', ['$scope', function canvasController($scope) {
+// app.run(function($rootScope) {
+// 	$rootScope.amount = 5;
+// });
+app.controller('drawCtrl', drawCtrl);
 
-}]);
-app.controller('formController', ['$scope', function($scope) {
-		
-}]);
+function drawCtrl() {
+	this.fLinesNumber=0;
+	this.fTrianglesNumber=0;
+	this.fSquaresNumber=0;
+	this.fCirclesNumber=0;
+	this.fHexagonsNumber=0;
+
+};
+drawCtrl.prototype.lines = function() {
+	this.linesNumber = Math.ceil(Math.random() * (this.fLinesNumber+1) -1);
+};
+drawCtrl.prototype.triangles = function() {
+	this.trianglesNumber =  Math.ceil(Math.random() * (this.fTrianglesNumber+1) -1);
+
+};
+drawCtrl.prototype.squares = function() {
+	this.squaresNumber =  Math.ceil(Math.random() * (this.fSquaresNumber+1) -1);
+
+};
+drawCtrl.prototype.circles = function() {
+	this.circlesNumber =  Math.ceil(Math.random() * (this.fCirclesNumber+1) -1);
+
+};
+drawCtrl.prototype.hexagons = function() {
+	this.hexagonsNumber =  Math.ceil(Math.random() * (this.fHexagonsNumber+1) -1);
+
+};
+drawCtrl.prototype.initialize = function() {
+	this.lines();
+	this.triangles();
+	this.squares();
+	this.circles();
+	this.hexagons();
+}
+drawCtrl.prototype.showInfo = function() {
+	return 'lines: ' + this.fLinesNumber +'/' + this.linesNumber + ', ' +
+			'triangles: ' + this.fTrianglesNumber + '/' + this.trianglesNumber + ', ' +
+			'squares: ' + this.fSquaresNumber + '/' + this.squaresNumber + ', ' +
+			'circles: ' + this.fCirclesNumber + '/' + this.circlesNumber + ', ' +
+			'hexagons: ' + this.fHexagonsNumber + '/' + this.hexagonsNumber;
+
+};
+
+drawCtrl.prototype.configCanvas = function() {
+		var canvas = document.getElementById("cnv");
+		var ctx = canvas.getContext("2d");
+		ctx.fillRect(50, 50, 250, 200);
+		ctx.strokeRect(50, 50, 250, 200);
+};
